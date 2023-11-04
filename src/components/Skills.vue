fbb1b602141f6673eff1a2b3f8492076ee35f3ab
Nehad Younis <nehadyounice@gmail.com>
2023-11-04 15:53:58 +0400

    skills images issue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -15,10 +15,10 @@ const data = [
     image: "/assets/code.png",
     title:'Programming languages',
     skills:[
-          {name:'Python', image:"/assets/skills/python.png"},
-          {name:'Java', image:"/assets/skills/java.png"},
-          {name:'C++', image:"/assets/skills/cpp.png"},
-          {name:'Dart', image:"/assets/skills/dart.png"},
+          {name:'Python', image:"/assets/python.png"},
+          {name:'Java', image:"/assets/java.png"},
+          {name:'C++', image:"/assets/cpp.png"},
+          {name:'Dart', image:"/assets/dart.png"},
         ]
   },
 
@@ -26,13 +26,13 @@ const data = [
     image: "/assets/comp.png",
     title:'Backend & Cloud',
     skills:[
-          {name:'AWS', image:"/assets/skills/aws.png"},
-          {name:'Django', image:"/assets/skills/django.png"},
-          {name:'FastAPI', image:"/assets/skills/fastapi.png"},
-          {name:'Docker', image:"/assets/skills/docker.png"},
-          {name:'GraphQL', image:"/assets/skills/dart.png"},
-          {name:'Git', image:"/assets/skills/git.png"},
-          {name:'Linux', image:"/assets/skills/linux.png"},
+          {name:'AWS', image:"/assets/aws.png"},
+          {name:'Django', image:"/assets/django.png"},
+          {name:'FastAPI', image:"/assets/fastapi.png"},
+          {name:'Docker', image:"/assets/docker.png"},
+          {name:'GraphQL', image:"/assets/dart.png"},
+          {name:'Git', image:"/assets/git.png"},
+          {name:'Linux', image:"/assets/linux.png"},
         ]
   },
 
@@ -40,10 +40,10 @@ const data = [
     image: "/assets/phone.png",
     title:'Mobile and Frontend',
     skills:[
-          {name:'Vuejs', image:"/assets/skills/vue.png"},
-          {name:'Flutter', image:"/assets/skills/flutter.png"},
-          {name:'HTML', image:"/assets/skills/html-5.png"},
-          {name:'CSS', image:"/assets/skills/css-3.png"},
+          {name:'Vuejs', image:"/assets/vue.png"},
+          {name:'Flutter', image:"/assets/flutter.png"},
+          {name:'HTML', image:"/assets/html-5.png"},
+          {name:'CSS', image:"/assets/css-3.png"},
         ]
   },
 
@@ -51,11 +51,11 @@ const data = [
     image: "/assets/db.png",
     title:'Databases',
     skills:[
-          {name:'MySQL', image:"src/assets/skills/mysql.png"},
-          {name:'PostgreSQL', image:"src/assets/skills/postgres.png"},
-          {name:'MongoDB', image:"src/assets/skills/mongo.png"},
-          {name:'Redis', image:"src/assets/skills/redis.png"},
-          {name:'Neo4j', image:"src/assets/skills/neo4j.png"},
+          {name:'MySQL', image:"src/assets/mysql.png"},
+          {name:'PostgreSQL', image:"src/assets/postgres.png"},
+          {name:'MongoDB', image:"src/assets/mongo.png"},
+          {name:'Redis', image:"src/assets/redis.png"},
+          {name:'Neo4j', image:"src/assets/neo4j.png"},
         ]
   },
 ]
